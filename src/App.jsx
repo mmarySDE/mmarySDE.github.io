7a36c9b3d95d10ae0ab43dc46f691a5937346a04
mmarySDE <mmary.SDE@gmail.com>
2022-02-05 18:51:24 -0600

    Add puzzle

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Iframe from "./Iframe";
+
+function App() {
+  return (
+    <div className="App">
+      <Iframe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Iframe.jsx
@@ -0,0 +1,14 @@
+function Iframe() {
+  return (
+    <div>
+      <iframe
+        src={
+          "https://www.jigsawexplorer.com/online-jigsaw-puzzle-player.html?frm=1&url=aHR0cHM6Ly9pLmltZ3VyLmNvbS9oZDA5ZU1iLnBuZw~~&nop=500&color=white"
+        }
+        height="600px"
+        width="85%"
+      />
+    </div>
+  );
+}
+export default Iframe;
